5d90fefc12880610fe11d57c7202df6dc065b9c6
Dnyaneshwar150 <dnyaneshwardimble25436@gmail.com>
2024-02-10 02:36:34 +0530

    adding navbar and footer ejs template creatd login and signup form

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
 const path = require('path');
+
+// Set the view engine to EJS
 app.set('view engine', 'ejs');
-app.set('path', path.join(__dirname, 'views'));
 
+// Set the views directory path
+app.set('views', path.join(__dirname, 'views'));
 
+// MongoDB connection URL
 let MONGO_URL = "mongodb://127.0.0.1:27017/majorProject";
+
+// Connect to MongoDB
 main().then(() => {
     console.log("Connected to DB");
-})
-.catch((err) => {
+}).catch((err) => {
     console.error(err);
 });
+
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-app.get("/",(req,res)=>{
-    res.send("Hi,I am  root");
+
+// Define route to render boilerplate.ejs
+app.get("/", (req, res) => {
+    res.render('./layouts/boilerplate.ejs'); // Correct
 });
 
-app.listen(8080,()=>{
-    console.log('listening on 8080');
+// Start the server
+app.listen(8080, () => {
+    console.log('Server is listening on port 8080');
 });
